5edc12fd8871a94eb0539b923088fb2fa0817f15
Sam-79 <sameerborkar79@gmail.com>
2022-07-27 19:09:30 +0530

    theme added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import CRMHeader from './components/CRMHeader'
 
 
+
+const customTheme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#923da0',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+    background: {
+      default: '#e0e0e0',
+    },
+    text: {
+      secondary: '#8c15a1',
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={customTheme}>
       <CRMHeader />
-    </>
+
+    </ThemeProvider>
   )
 }
 
